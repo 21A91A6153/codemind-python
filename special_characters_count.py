96bf016923f918efcabc2cb95d048f254970ccd5
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-05-31 11:54:49 +0530

    n=input() c=0 for i in n:     if ord(i)>=33 and ord(i)<=47:         c=c+1     elif ord(i)>=58 and ord(i)<=64:         c=c+1     elif ord(i)>=91 and ord(i)<=96:         c=c+1     elif ord(i)>=123 and ord(i)<=126:         c=c+1 print(c)     #32–47 / 58–64 / 91–96 / 123–126)

---
+++ special_characters_count.py
@@ -0,0 +1,13 @@
+n=input()
+c=0
+for i in n:
+    if ord(i)>=33 and ord(i)<=47:
+        c=c+1
+    elif ord(i)>=58 and ord(i)<=64:
+        c=c+1
+    elif ord(i)>=91 and ord(i)<=96:
+        c=c+1
+    elif ord(i)>=123 and ord(i)<=126:
+        c=c+1
+print(c)
+    #32–47 / 58–64 / 91–96 / 123–126)
