f37583aecd4093c55d1894433a5b47c6dfbc7396
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-07-12 11:38:35 +0530

    n=input() n=n.split() c=0 b='AEIOUaeiou' d="bcdfghjklmnpqrstvwxyz" for i in n:     if i[0] in b and i[-1] in d:         c=c+1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,8 +1,9 @@
-a=input()
-a=a.split()
+n=input()
+n=n.split()
 c=0
-for i in a:
-    if i[0] in 'aeiouAEIOU' and i[-1] in 'bcdfghjklmnpqrstvwxyz':
+b='AEIOUaeiou'
+d="bcdfghjklmnpqrstvwxyz"
+for i in n:
+    if i[0] in b and i[-1] in d:
         c=c+1
 print(c)
-    
