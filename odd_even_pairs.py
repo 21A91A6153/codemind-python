807b3adb39ee62ba89fe8aaf43e0755c8c16f6f6
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-09-07 19:51:38 +0530

    n=int(input()) l=list(map(int,input().split())) a=[] b=[] e=[] for i in l:     if i%2==0:         a.append(i)     else:         b.append(i) if len(a)<len(b):     for i in range(len(a)):         e.append(b[i])         e.append(a[i])     for i in range(len(a),len(b)):         e.append(b[i])     if n%2==0:         print(*e)     else:        e.append(0)        print(*e) if len(b)<len(a):     for i in range(len(b)):         e.append(b[i])         e.append(a[i])     for i in range(len(b),len(a)):         e.append(a[i])     if n%2==0:         print(*e)     else:         e.append(0)         print(*e) else:     for i in range(len(b)):         e.append(b[i])         e.append(a[i])     print(*e)

---
+++ odd_even_pairs.py
@@ -0,0 +1,40 @@
+n=int(input())
+l=list(map(int,input().split()))
+a=[]
+b=[]
+e=[]
+for i in l:
+    if i%2==0:
+        a.append(i)
+    else:
+        b.append(i)
+if len(a)<len(b):
+    for i in range(len(a)):
+        e.append(b[i])
+        e.append(a[i])
+    for i in range(len(a),len(b)):
+        e.append(b[i])
+    if n%2==0:
+        print(*e)
+    else:
+       e.append(0)
+       print(*e)
+if len(b)<len(a):
+    for i in range(len(b)):
+        e.append(b[i])
+        e.append(a[i])
+    for i in range(len(b),len(a)):
+        e.append(a[i])
+    if n%2==0:
+        print(*e)
+    else:
+        e.append(0)
+        print(*e)
+else:
+    for i in range(len(b)):
+        e.append(b[i])
+        e.append(a[i])
+    print(*e)
+        
+    
+        
