b4b7d1d28c091e42f1830762b7e5cca6bdf83a76
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-09-12 14:52:33 +0530

    n=input() m=input() n=n.lower() m=m.lower() n=set(n) m=set(m) l=n n=n.difference(m) m=m.difference(l) c=[] for i in n:     if i!=' ':         c.append(i) for i in m:     if i!=' ':         c.append(i) c=sorted(c) c="".join(c) print(len(c))

---
+++ Non_common_characters.py
@@ -0,0 +1,20 @@
+n=input()
+m=input()
+n=n.lower()
+m=m.lower()
+n=set(n)
+m=set(m)
+l=n
+n=n.difference(m)
+m=m.difference(l)
+c=[]
+for i in n:
+    if i!=' ':
+        c.append(i)
+for i in m:
+    if i!=' ':
+        c.append(i)
+c=sorted(c)
+c="".join(c)
+print(len(c))
+
