098bee171f9171b9a3e4de197f3b129c45dfeee4
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-09-18 19:23:34 +0530

    n=input() n=n.lower() n=n.split() k=[] s=n[0] for i in s:     for j in n:         if i in j:             continue         else:             break     else:         k.append(i) if k==[]:     print(-1) else:     k=sorted(k)     print(k[0])

---
+++ Common_characters_-_3.py
@@ -0,0 +1,19 @@
+n=input()
+n=n.lower()
+n=n.split()
+k=[]
+s=n[0]
+for i in s:
+    for j in n:
+        if i in j:
+            continue
+        else:
+            break
+    else:
+        k.append(i)
+if k==[]:
+    print(-1)
+else:
+    k=sorted(k)
+    print(k[0])
+
